a98802cfc6f6f3ebe27be754d3719360124250df
Filipo <filipfilip280@gmail.com>
2023-05-10 10:27:54 +0200

    init

---
+++ packages/eslint-config-custom/index.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["turbo", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+};
