78c368c588d57bdc28f5e1e0ed238322954d98a4
Filipo <filipfilip280@gmail.com>
2023-05-10 15:30:36 +0200

    feat(reactivity): create effect

---
+++ packages/reactivity/src/effect/effect.test.ts
@@ -0,0 +1,44 @@
+import { describe, expect, test } from "vitest";
+import { effect } from "./effect";
+import { context } from "../shared";
+
+describe("effect", () => {
+  test("create effect", () => {
+    let check = false;
+    effect(() => {
+      check = true;
+    });
+
+    expect(check).toBe(true);
+  });
+
+  test("context length in effect function", () => {
+    expect(context.length).toBe(0);
+
+    effect(() => {
+      expect(context.length).toBe(1);
+    });
+
+    expect(context.length).toBe(0);
+  });
+
+  test("nested effects", () => {
+    expect(context.length).toBe(0);
+
+    effect(() => {
+      expect(context.length).toBe(1);
+
+      effect(() => {
+        expect(context.length).toBe(2);
+
+        effect(() => {
+          expect(context.length).toBe(3);
+        });
+      });
+
+      expect(context.length).toBe(1);
+    });
+
+    expect(context.length).toBe(0);
+  });
+});
---
+++ packages/reactivity/src/effect/effect.ts
@@ -0,0 +1,24 @@
+import { context } from "../shared";
+import { Subscriber } from "../types";
+
+function cleanup(subscriber: Subscriber) {
+  for (const dependency of subscriber.dependencies) {
+    dependency.delete(subscriber);
+  }
+
+  subscriber.dependencies.clear();
+}
+
+export function effect(fn: () => void) {
+  const subscriber: Subscriber = {
+    execute() {
+      cleanup(subscriber);
+      context.push(subscriber);
+      fn();
+      context.pop();
+    },
+    dependencies: new Set(),
+  };
+
+  subscriber.execute();
+}
