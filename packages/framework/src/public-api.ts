5c5738c3b250276a3617d4f92241ee0ea57c2b0f
Filipo <filipfilip280@gmail.com>
2023-05-12 08:32:23 +0200

    feat(framework): export children type

--- packages/framework/src/public-api.ts
+++ packages/framework/src/public-api.ts
@@ -1,3 +1,4 @@
 export {Fragment, createElement} from './jsx-transform/jsx-transform'
 export {render} from './render'
 export {effect, memo, signal} from '@filipo/reactivity'
+export type {Children} from './jsx-transform/types'
