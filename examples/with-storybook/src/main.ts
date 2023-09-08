5b5175aea5d7e72f565cd45f9536e7923a9df44e
Filipo <filipfilip280@gmail.com>
2023-09-08 23:32:04 +0200

    fix(examples/with-storybook): more accurate error message

--- examples/with-storybook/src/main.ts
+++ examples/with-storybook/src/main.ts
@@ -4,6 +4,6 @@ import './style.css';
 import { App } from './app';
 
 const app = document.getElementById('app');
-if (!app) throw Error('app not exist');
+if (!app) throw Error('app element not exist');
 
 render(app, App);
